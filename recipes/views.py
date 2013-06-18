abf08b10284851fb7165684a596bc072c50254f3
Ben Blaut <blautben@msu.edu>
2013-06-18 17:08:06 -0400

    Some inventory checking functionality

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,80 @@
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from recipes.models import Recipes, Ingredient
+from bottles.models import Bottle
 from django.template import RequestContext
+import unicodedata
 
 def recipes(request):
     recipes_list = Recipes.objects.all()
-    return render_to_response('recipes/recipes.html', {'recipes_list': recipes_list,}, 
-                              context_instance=RequestContext(request))
-    
+    inventory = Bottle.objects.filter(user=request.user)
+
+    types_needed = []
+    ingredients_needed = []
+
+    for rec in recipes_list:
+        ings = rec.ingredients.all()
+        for i in ings:
+            brands_owned = []
+            amt = convert_to_ml(i.amount)
+            typ = i.part
+            brands_owned = check_inventory_for_type(inventory, typ, brands_owned)
+            if len(brands_owned) == 0:
+                types_needed.append((typ, amt))
+            else:
+                amounts_from_brands = []
+
+                for (m, l) in brands_owned:
+                    amount_owned_of_brand = get_liquor_amount(inventory, m, l)
+                    amounts_from_brands.append(amount_owned_of_brand)
+                if (amount_owned_of_brand <= amt):
+                    amount_needed = amt - amount_owned_of_brand
+                    ingredients_needed.append((unicodedata.normalize('NFKD', typ).encode('ascii', 'ignore'), amount_needed))
+
+    for (type_needed, amount_needed) in types_needed:
+        ingredients_needed.append((unicodedata.normalize('NFKD', type_needed).encode('ascii', 'ignore'), amount_needed))    
+
+    return render_to_response('recipes/recipes.html', {'recipes_list': recipes_list, 'ingredients_needed': ingredients_needed,}, 
+                              context_instance=RequestContext(request))    
+
+def convert_to_ml(amount):
+    "Take a string of form (# unit), convert the # to ml and change unit to ml"
+    amount_split = amount.split()
+    float_amount = float(amount_split[0])
+            
+    if amount_split[1] == "ml" or amount_split[1] == "milliliter" or amount_split[1] == "milliliters":
+        return float_amount
+    elif amount_split[1] == "l" or amount_split[1] == "liter" or amount_split[1] == "liters":
+        float_amount *= 1000
+        return float_amount
+    elif amount_split[1] == "oz" or amount_split[1] == "ounce" or amount_split[1] == "ounces":
+        float_amount *= 29.5735
+        return float_amount
+    elif amount_split[1] == "gallons" or amount_split[1] == "gallon" or amount_split[1] == "g":
+        float_amount *= 3785.41
+        return float_amount
+    else:
+        print "\nIncorrect unit of measurement, use ml, g, or oz."
+
+def check_inventory_for_type(inventory, typ, brands_owned):
+    for b in inventory:
+        if b.typ == typ:
+            brands_owned.append((b.mfg, b.liquor))
+
+    return brands_owned
+
+def get_liquor_amount(inventory, mfg, liquor):
+    "Retrieve the total amount of any given liquor currently in inventory."
+    amounts = [] 
+    found = False
+    for b in inventory:
+        if b.mfg == mfg and b.liquor == liquor:
+            found = True
+            ml_amount = convert_to_ml(b.amount)           
+            amounts.append(ml_amount)
+            total = sum(amounts)
+            
+            return total
+
+    if not found:
+        err = "Missing liquor: manufacturer '%s', name '%s'" % (mfg, liquor)
+        raise LiquorMissing(err)
